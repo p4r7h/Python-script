393c4797d558624d2b7e3c3d60b83669c1092c92
robon <37813830+p4r7h@users.noreply.github.com>
2021-06-01 02:07:51 +0530

    Create Bytes.py

---
+++ cryptohack/Bytes.py
@@ -0,0 +1,6 @@
+# Bytes and Big Integers
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import long_to_bytes
+
+a = b'11515195063862318899931685488813747395775516287289682636499965282714637259206269'
+print (long_to_bytes(a))
